1202b01958338f98dc0fc9a02a1d797318a62da2
Ralf Anton Beier <ralf_beier@me.com>
2018-12-25 17:15:24 +0100

    Added testcase for mac os x.

---
+++ .travis/install.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+if [[ $TRAVIS_OS_NAME == 'osx' ]]; then
+
+    # Install some custom requirements on OS X
+    # e.g. brew install pyenv-virtualenv
+
+    case "${TOXENV}" in
+        py27)
+            # Install some custom Python 3.2 requirements on OS X
+            ;;
+    esac
+else
+  - rm setup.py
+  - mv setup.py.travis setup.py
+  - rm requirements.txt
+  - mv requirements.txt.travis requirements.txt
+    # Install some custom requirements on Linux
+fi
+
+python setup.py install
+pip install -r requirements.txt
+
+which python
+which pip
+py.test --version
